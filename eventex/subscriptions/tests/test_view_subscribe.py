79da7abdcad8de703f57cb7ec20bfb1fb7ec4252
Caio Nickel <caio.nickel@gmail.com>
2022-03-15 11:39:50 -0300

    Criacao de thank you page Subscription

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -45,8 +45,8 @@ class SubscribePostValid(TestCase):
         self.resp = self.client.post('/inscricao/', data)
 
     def test_post(self):
-        """Valid POST should redirect to /inscricao/"""
-        self.assertEqual(302, self.resp.status_code)
+        """Valid POST should redirect to /inscricao/1/"""
+        self.assertRedirects(self.resp, '/inscricao/1/')
 
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
@@ -76,14 +76,3 @@ class SubscribePostInvalid(TestCase):
 
     def test_do_not_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
-
-
-class SubscribeSuccessMessage(TestCase):
-    def test_message(self):
-        data = dict(name='Caio Nickel', cpf='12345678901', email='caio.nickel@gmail.com',
-                    phone='43-99682-5355')
-
-        response = self.client.post('/inscricao/', data, follow=True)
-        self.assertContains(response, 'Inscrição realizada com sucesso!')
-
-
