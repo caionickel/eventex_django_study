4d79830206496dccc8b88ea1da99b5d05713df3b
Caio Nickel <caio.nickel@gmail.com>
2022-03-30 16:27:30 -0300

    Speaker

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import eventex.core.views
+from eventex.core.views import home, speaker_detail
 
 
 urlpatterns = [
-    path('', eventex.core.views.home, name='home'),
+    path('', home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
+    path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
