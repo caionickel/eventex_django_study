d1a82d2ec8f4137b5a97b674fbda8ebcad606136
Caio Nickel <caio.nickel@gmail.com>
2022-04-04 14:55:58 -0300

    Adiciona talks

--- eventex/urls.py
+++ eventex/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from eventex.core.views import home, speaker_detail
+from eventex.core.views import home, speaker_detail, talk_list
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('inscricao/', include('eventex.subscriptions.urls')),
+    path('palestras/', talk_list, name='talk_list'),
     path('palestrantes/<slug:slug>/', speaker_detail, name='speaker_detail'),
     path('admin/', admin.site.urls),
 ]
