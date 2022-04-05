04fde6869b3f6003eb87bb42cdcbd9b5f2120258
Caio Nickel <caio.nickel@gmail.com>
2022-04-05 17:24:46 -0300

    Adiciona Managers

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -8,10 +8,11 @@ class ContactInLine(admin.TabularInline):
     model = Contact
     extra = 1
 
+
 class SpeakerModelAdmin(admin.ModelAdmin):
     inlines = [ContactInLine]
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ['name', 'photo_img', 'website_link']
+    list_display = ['name', 'photo_img', 'website_link', 'email', 'phone']
 
     def website_link(self, obj):
         return format_html('<a href="{0}">{0}</a>', obj.website)
@@ -22,6 +23,16 @@ class SpeakerModelAdmin(admin.ModelAdmin):
         return format_html('<img width="32px" src="{}" />', obj.photo)
 
     photo_img.short_description = 'foto'
+
+    def email(self, obj):
+        return obj.contact_set.emails().first()
+
+    email.short_description = 'e-mail'
+
+    def phone(self, obj):
+        return obj.contact_set.phones().first()
+
+    phone.short_description = 'telefone'
 
 
 admin.site.register(Speaker, SpeakerModelAdmin)
